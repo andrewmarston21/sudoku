a2aab591d7c95d4d7808122680f228c7b973111f
marstonad20 <marstonad20@gcc.edu>
2024-07-12 11:10:23 -0400

    (sort of) rolled back verbosity changes

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ This is a Python implementation of a backtracking seach-based Sudoku-solving alg
 ___
 
 Run `main.py` from the command line with the path of the board you want to solve as the first command line argument after the name of the program. Make sure to include `csvs/` at the start of the board's path name.\
-Set the verbosity of the output as the second argument.\
+Set the verbosity of the output in `main.py`.\
 If you have a board you'd like the algorithm to solve, use one of the included boards as a reference. (`0` in the board file represents an empty square on a visual Sudoku board.)\
--- main.py
+++ main.py
@@ -6,14 +6,13 @@ from csvs import read_csv
 if __name__ == "__main__":
     """handles inputting a board and outputting a representation of that board solved"""
     path: str = sys.argv[1]
-    verbose = False
-    if len(sys.argv) >= 3:
-        verbose = bool(sys.argv[2])
+    verbosity = True
+    
     board: list[list[int]] = read_csv(path)
     print("The board to solve:")
     print_board(board)
     print("\n")
-    solved_board: list[list[int]] = backtrack_solver(board, verbose)
+    solved_board: list[list[int]] = backtrack_solver(board, verbose=verbosity)
     if solved_board == None: 
         print("Failed to solve the board")
     else:
