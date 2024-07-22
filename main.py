23213144ef3b75b293a9a440862eb38b9a086a50
marstonad20 <marstonad20@gcc.edu>
2024-07-22 11:37:05 -0400

    Program now prints how long it took to solve a board.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from time import time
 
 from backtrack import backtrack_solver, print_board
 from csvs import read_csv
@@ -6,14 +7,19 @@ from csvs import read_csv
 if __name__ == "__main__":
     """handles inputting a board and outputting a representation of that board solved"""
     path: str = sys.argv[1]
-    verbosity = True
+    verbosity = False
     board: list[list[int]] = read_csv(path)
     print("The board to solve:")
     print_board(board)
     print("\n")
+    print("This may take several minutes:")
+    start_time = time()
     solved_board: list[list[int]] = backtrack_solver(board, verbose=verbosity)
+    end_time = time()
+    elapsed_time = round(end_time - start_time, 2)
     if solved_board is None:
         print("Failed to solve the board")
     else:
         print("The solved board:")
         print_board(solved_board)
+    print("The program took " + str(elapsed_time) + " seconds to run")
